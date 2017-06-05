a62f22591be66c21862632cd5e70854f1a7d43a8
handiono <handiono87@gmail.com>
2017-06-05 14:52:02 +0700

    change to local server

--- db_config.php
+++ db_config.php
@@ -4,8 +4,9 @@
  * All database connection variables
  */
  
-define('DB_USER', "handiono"); // db user
-define('DB_PASSWORD', "Xzo@39k4"); // db password (mention your db password here)
+define('DB_USER', "ip"); // db user
+define('DB_PASSWORD', "IndahPur1"); // db password (mention your db password here)
 define('DB_DATABASE', "aplikasi_indahpuri"); // database name
-define('DB_SERVER', "192.168.23.208"); // db server
+define('DB_SERVER', "192.168.1.99");
+//define('DB_SERVER', "192.168.23.208"); // db server ini cloudkilat punya
 ?>
