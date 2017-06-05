c11d262739677cbe0570ad704f138e2524c9c98f
handiono <handiono87@gmail.com>
2017-06-05 15:09:31 +0700

    change db

--- db_config.php
+++ db_config.php
@@ -8,5 +8,4 @@ define('DB_USER', "ip"); // db user
 define('DB_PASSWORD', "IndahPur1"); // db password (mention your db password here)
 define('DB_DATABASE', "aplikasi_indahpuri"); // database name
 define('DB_SERVER', "192.168.1.99");
-//define('DB_SERVER', "192.168.23.208"); // db server ini cloudkilat punya
 ?>
