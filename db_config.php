8bea2a08d35333db110fc94af9e8e7f6d571f62f
handiono <handiono87@gmail.com>
2017-06-05 16:01:33 +0700

    change user

--- db_config.php
+++ db_config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
  
-define('DB_USER', "ip"); // db user
-define('DB_PASSWORD', "IndahPur1"); // db password (mention your db password here)
+define('DB_USER', "escurity-user"); // db user
+define('DB_PASSWORD', "123"); // db password (mention your db password here)
 define('DB_DATABASE', "aplikasi_indahpuri"); // database name
 define('DB_SERVER', "192.168.1.99");
 ?>
