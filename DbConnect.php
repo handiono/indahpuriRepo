c5fa0825c03b6cd201ca5f7d139720d239ff3291
handiono <handiono87@gmail.com>
2017-06-05 16:17:49 +0700

    simple fix mysql error

--- DbConnect.php
+++ DbConnect.php
@@ -25,10 +25,10 @@ class DB_CONNECT {
         require_once __DIR__ . '/db_config.php';
  
         // Connecting to mysql database
-        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE) or die(mysqli_error($con));
+        $con = @mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());
  
         // Selecing database
-        //$db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error());
+        $db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error());
  
         // returing connection cursor
         return $con;
@@ -39,7 +39,7 @@ class DB_CONNECT {
      */
     function close() {
         // closing db connection
-        mysqli_close();
+        mysql_close();
     }
  
 }
